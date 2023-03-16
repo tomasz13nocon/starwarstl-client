e80e9a5762a6c166cfe1fd8f5ccfce39e0f7263c
tomasz13nocon <tomasz13nocon@gmail.com>
2023-03-16 22:01:41 +0100

    removed ga, updated head

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   // build.outDir and publicDir are relative to the `root`
