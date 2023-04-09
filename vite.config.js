eae54333eae048bf1e012a2eaee8e10a3f3ae35a
tomasz13nocon <tomasz13nocon@gmail.com>
2023-04-09 13:22:21 +0200

    update env

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { ViteAliases } from 'vite-aliases';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { ViteAliases } from "vite-aliases";
 
 export default defineConfig({
   // build.outDir and publicDir are relative to the `root`
-  root: 'src',
+  root: "src",
+  envDir: "../",
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
   },
-  publicDir: '../public',
+  publicDir: "../public",
   plugins: [react({}), ViteAliases()],
-})
+});
