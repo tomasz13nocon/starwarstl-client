b9a6ed5c761a991c46ead422a799ea8d7a60630c
tomasz13nocon <tomasz13nocon@gmail.com>
2024-08-12 18:27:43 +0200

    fix shell margins

--- src/layouts/shell.jsx
+++ src/layouts/shell.jsx
@@ -1,5 +1,6 @@
+import clsx from "clsx";
 import c from "./styles/shell.module.scss";
 
-export default function Shell({ children }) {
-  return <div className={c.shell}>{children}</div>;
+export default function Shell({ children, noMargin }) {
+  return <div className={clsx(c.shell, noMargin && c.noMargin)}>{children}</div>;
 }
--- src/pages/timeline/timeline.jsx
+++ src/pages/timeline/timeline.jsx
@@ -164,7 +164,7 @@ export default function Timeline() {
   );
 
   return (
-    <Shell>
+    <Shell noMargin>
       <div className="circle-buttons">
         <Legend />
         <Search
